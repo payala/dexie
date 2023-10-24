7d32cee294778a0a124885dcf2b71363483bf142
Pedro Perez de Ayala <ppayala@gmail.com>
2023-10-24 07:56:33 +0200

    Fix banner animation

--- src/Components/MessageBanner.js
+++ src/Components/MessageBanner.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 function MessageBanner({ message, type, onClose, withTimeout }) {
+  const [state, setState] = React.useState("hidden");
+  const appearTime = 100;
+  const visibleTime = 5000;
+  const fadeTime = 700;
+
   const bgColor =
     type === "error"
       ? "bg-red-500"
@@ -9,16 +14,27 @@ function MessageBanner({ message, type, onClose, withTimeout }) {
       : "bg-yellow-500";
 
   React.useEffect(() => {
-    if (withTimeout) {
-      const timer = setTimeout(onClose, 5000); // Auto-hide after 5 seconds
-      return () => clearTimeout(timer); // Clear the timer if the component is unmounted
+    let timer;
+    if (state === "hidden" && message) {
+      timer = setTimeout(() => {
+        setState("visible");
+      }, appearTime);
+    } else if (state === "visible" && withTimeout) {
+      timer = setTimeout(() => {
+        setState("hiding");
+      }, visibleTime);
+    } else if (state === "hiding") {
+      timer = setTimeout(() => {
+        onClose();
+      }, fadeTime);
     }
-  }, [onClose, withTimeout]);
+    return () => clearTimeout(timer); // Clear the timer if the component is unmounted
+  }, [message, onClose, state, withTimeout]);
 
   return (
     <div
       className={`fixed top-0 left-0 w-full p-4 ${bgColor} text-white text-center transition-transform transform ease-in-out duration-700 ${
-        message ? "translate-y-0" : "-translate-y-full"
+        state === "visible" ? "translate-y-0" : "-translate-y-full"
       }`}
       role="alert"
     >
