cb481fd79f88734b82f815974f3fb9341b3a8efd
Pedro Perez de Ayala <ppayala@gmail.com>
2023-11-07 13:44:13 +0100

    fix private key format for hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const privateKey = process.env.PRIVATE_KEY || "";
+const privateKey = process.env.PRIVATE_KEY.split("#")[0].trim() || "";
+console.log(`private key: ${privateKey}!!!`);
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -16,6 +17,12 @@ module.exports = {
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       chainId: 5,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      chainId: 1,
+      accounts: [privateKey],
     },
   },
 };
